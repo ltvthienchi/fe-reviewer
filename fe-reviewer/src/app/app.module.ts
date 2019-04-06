f4590d9996d289f82aaf111e8fb73f24ed7f07dd
Thiện Chí <ltvthienchi@gmail.com>
2019-04-06 16:53:26 +0700

    // update

--- fe-reviewer/src/app/app.module.ts
+++ fe-reviewer/src/app/app.module.ts
@@ -3,26 +3,26 @@ import { NgModule } from '@angular/core';
 import { NotifierModule, NotifierOptions } from 'angular-notifier';
 //
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CompanyComponent } from './company/company.component';
-import { ReviewerComponent } from './reviewer/reviewer.component';
-import { AdminComponent } from './admin/admin.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { MenuComponent } from './menu/menu.component';
-import { DetailReviewerComponent } from './reviewer/detail/detail.component';
-import { UpdateInfoReviewerComponent } from './reviewer/update-info-reviewer/update-info-reviewer.component';
-import { FeedbackReviewerComponent } from './reviewer/feedback-reviewer/feedback-reviewer.component';
-import { ViewHistoryReviewerComponent } from './reviewer/view-history-reviewer/view-history-reviewer.component';
-import { RegisterCompanyComponent } from './reviewer/register-company/register-company.component';
-import { DetailCompanyComponent } from './company/detail-company/detail-company.component';
-import { ViewHistoryCompanyComponent } from './company/view-history-company/view-history-company.component';
-import { StatsComponent } from './company/stats/stats.component';
-import { ReportAdminComponent } from './admin/report-admin/report-admin.component';
-import { FeedbackAdminComponent } from './admin/feedback-admin/feedback-admin.component';
-import { CustomerCareAdminComponent } from './admin/customer-care-admin/customer-care-admin.component';
-import { LockReviewerComponent } from './admin/lock-reviewer/lock-reviewer.component';
-import { ComfirmCompanyComponent } from './admin/comfirm-company/comfirm-company.component';
+import { HomeComponent } from './component/home/home.component';
+import { CompanyComponent } from './component/company/company.component';
+import { ReviewerComponent } from './component/reviewer/reviewer.component';
+import { AdminComponent } from './component/admin/admin.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { DetailReviewerComponent } from './component/reviewer/detail/detail.component';
+import { UpdateInfoReviewerComponent } from './component/reviewer/update-info-reviewer/update-info-reviewer.component';
+import { FeedbackReviewerComponent } from './component/reviewer/feedback-reviewer/feedback-reviewer.component';
+import { ViewHistoryReviewerComponent } from './component/reviewer/view-history-reviewer/view-history-reviewer.component';
+import { RegisterCompanyComponent } from './component/reviewer/register-company/register-company.component';
+import { DetailCompanyComponent } from './component/company/detail-company/detail-company.component';
+import { ViewHistoryCompanyComponent } from './component/company/view-history-company/view-history-company.component';
+import { StatsComponent } from './component/company/stats/stats.component';
+import { ReportAdminComponent } from './component/admin/report-admin/report-admin.component';
+import { FeedbackAdminComponent } from './component/admin/feedback-admin/feedback-admin.component';
+import { CustomerCareAdminComponent } from './component/admin/customer-care-admin/customer-care-admin.component';
+import { LockReviewerComponent } from './component/admin/lock-reviewer/lock-reviewer.component';
+import { ComfirmCompanyComponent } from './component/admin/comfirm-company/comfirm-company.component';
 
 const customNotifierOptions: NotifierOptions = {
   position: {
