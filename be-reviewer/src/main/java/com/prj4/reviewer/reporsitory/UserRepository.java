e710234907f5f4817211fca42c4b7371f8e38f9a
Vu Ho <hohoanvu1993@gmail.com>
2019-04-27 18:07:33 +0700

    April_27-commit revert back-end

---
+++ be-reviewer/src/main/java/com/prj4/reviewer/reporsitory/UserRepository.java
@@ -0,0 +1,11 @@
+package com.prj4.reviewer.reporsitory;
+
+import com.prj4.reviewer.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, String> {
+
+    User findByUserName(String userName);
+}
