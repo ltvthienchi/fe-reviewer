4e0326b4515eef3d7479e6c1fbb100162bdc9959
Vu Ho <hohoanvu1993@gmail.com>
2019-04-30 16:27:08 +0700

    Commit Test Mode + Change Home + Login

--- fe-reviewer/src/app/component/login/login.component.ts
+++ fe-reviewer/src/app/component/login/login.component.ts
@@ -3,6 +3,12 @@ import { Router } from '@angular/router';
 import {UserService} from '../../services/user-service/user.service';
 import {NotifierService} from 'angular-notifier';
 import {JwtHelperService} from '@auth0/angular-jwt';
+import {
+  validatorEmail,
+  validatorRequired,
+  validatorPassword,
+} from '../../services/validator/validator';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,36 +18,50 @@ import {JwtHelperService} from '@auth0/angular-jwt';
 export class LoginComponent implements OnInit {
   private notifier: NotifierService;
   private jwtHelper: JwtHelperService;
-  constructor(private userService: UserService, private router: Router, notifier: NotifierService, jwtHelper: JwtHelperService) {
+  signInForm: FormGroup;
+  validatorForm = {
+    signInForm: true
+  };
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router,
+              notifier: NotifierService, jwtHelper: JwtHelperService) {
     this.notifier = notifier;
     this.jwtHelper = jwtHelper;
   }
 
   ngOnInit() {
+    this.signInForm = this.formBuilder.group({
+      email: ['', [validatorRequired, validatorEmail]],
+      password: ['', [validatorPassword]]
+    });
   }
 
-  OnSubmit(userName, password) {
-    this.userService.userAuthentication(userName, password).subscribe((data: any) => {
-      if (data.status === 'FAIL') {
-        this.showNotification('error', data.result);
-      } else {
-        const tokenDecoded = this.jwtHelper.decodeToken(data.result);
-        localStorage.setItem('userToken', data.result);
-        if (tokenDecoded.scopes.authority === 'ROLE_COMPANY') {
-          localStorage.setItem('typeRev', '1');
+  OnSubmit() {
+    if (this.signInForm.status === 'INVALID') {
+      this.validatorForm.signInForm = false;
+    } else {
+      this.userService.userAuthentication(this.signInForm.value.email,
+        this.signInForm.value.password).subscribe((data: any) => {
+        if (data.status === 'FAIL') {
+          this.showNotification('error', data.result);
         } else {
-          localStorage.setItem('typeRev', '2');
-        }
-        localStorage.setItem('fullName', tokenDecoded.fullName);
-        localStorage.setItem('isActive', tokenDecoded.isActive);
-        if (tokenDecoded.isActive !== true) {
-          this.showNotification('error', 'Account is not active!!');
-        } else {
-          this.router.navigate(['/home']);
-        }
+          const tokenDecoded = this.jwtHelper.decodeToken(data.result);
+          localStorage.setItem('userToken', data.result);
+          if (tokenDecoded.scopes.authority === 'ROLE_COMPANY') {
+            localStorage.setItem('typeRev', '1');
+          } else {
+            localStorage.setItem('typeRev', '2');
+          }
+          localStorage.setItem('fullName', tokenDecoded.fullName);
+          localStorage.setItem('isActive', tokenDecoded.isActive);
+          if (tokenDecoded.isActive !== true) {
+            this.showNotification('error', 'Account is not active!!');
+          } else {
+            this.router.navigate(['/home']);
+          }
 
-      }
-    });
+        }
+      });
+    }
   }
 
   public showNotification(type: string, message: string): void {
