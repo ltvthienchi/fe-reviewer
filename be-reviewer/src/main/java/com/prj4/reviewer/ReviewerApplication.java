9e64dbe3d7abb4111043dc0e2cbe7565d5557a5d
Vu Ho <hohoanvu1993@gmail.com>
2019-05-04 09:13:47 +0700

    commit - upload file

--- be-reviewer/src/main/java/com/prj4/reviewer/ReviewerApplication.java
+++ be-reviewer/src/main/java/com/prj4/reviewer/ReviewerApplication.java
@@ -1,11 +1,16 @@
 package com.prj4.reviewer;
 
+import com.prj4.reviewer.core.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class ReviewerApplication {
 
     public static void main(String[] args) {
