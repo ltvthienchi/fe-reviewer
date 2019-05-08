af760adf8dc1733b7052b17ea156da8127af74bf
nguyenhuudam <nguyenhuudam0801@gmail.com>
2019-05-08 23:24:58 +0700

    update

--- fe-reviewer/src/app/component/admin/manage-admin/manage-admin.component.ts
+++ fe-reviewer/src/app/component/admin/manage-admin/manage-admin.component.ts
@@ -9,6 +9,7 @@ import {
 import { NotifierService } from 'angular-notifier';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { HttpService } from '../../../services/http/http.service';
+import { Admin } from '../../../model/admin.model';
 @Component({
   selector: 'app-manage-admin',
   templateUrl: './manage-admin.component.html',
@@ -16,7 +17,11 @@ import { HttpService } from '../../../services/http/http.service';
 })
 export class ManageAdminComponent implements OnInit {
   private notifier: NotifierService;
-  listAdmin= [] ;
+  private dataEdit = {
+    fullNameAdmin: ''
+  };
+  
+  listAdmin : Admin[] ;
   adminForm: FormGroup;
   validatorForm = {
     adminForm: true
@@ -30,21 +35,41 @@ export class ManageAdminComponent implements OnInit {
 
 
     this.adminForm = this.formBuilder.group({
-      titleFeedback: ['', [validatorRequired]],
-      contentFeedback: ['', [validatorRequired]],
+      idAdmin:[],
+      dtCreated:[],
+      active:[],
+      fullNameAdmin: ['', [validatorRequired]],
+      dobAdmin: ['', [validatorRequired]],
+      emailAdmin: ['', [validatorRequired]],
+      addressAdmin: ['', [validatorRequired]],
+      phoneAdmin: ['', [validatorRequired]],
+      passAdmin: ['', [validatorRequired]]
       
     });
 
     this.httpService.getAllAdmin().subscribe((data: any) => {
       
-      console.log(data);
+     // console.log(data);
       this.listAdmin = data;
+      console.log(this.listAdmin);
       
     });
   }
 
   public showNotification(type: string, message: string): void {
     this.notifier.notify(type, message);
+  }
+
+  public editAdmin(admin : any) {
+    admin.dobAdmin
+    this.adminForm.setValue(admin);
+    this.adminForm.setValue({
+      dobAdmin: admin.dobAdmin.toISOString().slice(0,10)
+    })
+
+    this.dataEdit = admin;
+    console.log(this.adminForm);
+    //console.log(this.dataEdit);
   }
 
 }
