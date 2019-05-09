23e2add5a611ff065a7ddeb09c3af37c0209454b
nguyenhuudam <nguyenhuudam0801@gmail.com>
2019-05-09 18:35:16 +0700

    update admin model

---
+++ fe-reviewer/src/app/model/admin.model.ts
@@ -0,0 +1,12 @@
+export class Admin {
+    idAdmin: string;
+    fullNameAdmin: string;
+    emailAdmin: string;
+    passAdmin: string;
+    dobAdmin: Date;;
+    isActive: boolean;
+    addressAdmin: string;
+    phoneAdmin: string;
+    dtCreated: Date;
+    aaa:any;
+  }
