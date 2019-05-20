08c28efca16889b3b49c088e576ac742309c23ac
Vu Ho <hohoanvu1993@gmail.com>
2019-05-20 18:41:19 +0700

    commit off test mode

--- be-reviewer/src/main/java/com/prj4/reviewer/core/Constants.java
+++ be-reviewer/src/main/java/com/prj4/reviewer/core/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String SIGNING_KEY = "devglan123r";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final boolean IS_TEST_MODE = true;
+    public static final boolean IS_TEST_MODE = false;
 
     // Replace with your email here:
     public static final String MY_EMAIL = "bestchoicesmartcmp@gmail.com";
